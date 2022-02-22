d20591c2c70f60588d8da491b636edcd1f352947
Brayden O'Neal <boneal005@drury.edu>
2022-02-22 15:34:44 -0600

    Added mics.xml for project setup

--- src/main/java/edu/drury/mandelbrotnavigator/math/MandelbrotMath.java
+++ src/main/java/edu/drury/mandelbrotnavigator/math/MandelbrotMath.java
@@ -5,8 +5,8 @@ public class MandelbrotMath {
 	private MandelbrotMath() {}
 
 	public static int getMandelbrotValue(double a, double b, int iterations) {
-		double x = 0;
-		double y = 0;
+		double x = a;
+		double y = b;
 
 		for (int i = 0; i < iterations; i++) {
 			double px = x;
